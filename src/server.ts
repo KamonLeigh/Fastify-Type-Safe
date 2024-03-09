011d2501d7e40276613a677df608385e8d166a32
Byron Dunkley <leigh@Byrons-MacBook-Air.local>
2024-03-09 16:50:41 +0000

    add src/server.ts and set up route and main file

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import { join } from "node:path";
+import Fastify from "fastify";
+import AutLoad from "@fastify/autoload";
+
+const fastify = Fastify({ logger: true });
+
+void fastify.register(AutLoad, {
+  dir: join(__dirname, "routes"),
+});
+
+fastify.listen({ host: "0.0.0.0", port: 3000 }).catch((err) => {
+  fastify.log.error(err);
+  process.exit(1);
+});
