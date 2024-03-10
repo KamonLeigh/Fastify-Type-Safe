a553c3fa2865bae99406703c85cc2d688c2c5254
Byron Dunkley <leigh@Byrons-MacBook-Air.local>
2024-03-10 18:09:02 +0000

    add swagger/ swagger ui

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -1,50 +1,56 @@
 import {
   type FastifyPluginAsyncTypebox,
-  Type,
-} from "@fastify/type-provider-typebox";
+  Type
+} from '@fastify/type-provider-typebox'
 
 const plugin: FastifyPluginAsyncTypebox = async function (fastify, opts) {
   fastify.get(
-    "/",
+    '/',
     {
       schema: {
+        tags: ['Hello World'],
+        description: 'Salute someone via GET call',
+        summary: 'GET Hello Route',
         querystring: Type.Object({
-          name: Type.String({ default: "world" }),
+          name: Type.String({ default: 'world' })
         }),
         response: {
           200: Type.Object({
-            hello: Type.String(),
-          }),
-        },
-      },
+            hello: Type.String()
+          })
+        }
+      }
     },
     (req) => {
-      const { name } = req.query;
-      return { hello: name };
-    },
-  );
+      const { name } = req.query
+      return { hello: name }
+    }
+  )
 
   fastify.post(
-    "/",
+    '/',
     {
       schema: {
+        tags: ['Hello World'],
+        description: 'Salute someone via a POST call',
+        summary: 'POST Hello Route',
         body: Type.Object({
-          name: Type.Optional(Type.String()),
+          name: Type.Optional(Type.String())
         }),
         response: {
           200: Type.Object({
-            hello: Type.String(),
-          }),
-        },
-      },
+            hello: Type.String()
+          })
+        }
+      }
     },
     async (req) => {
-      const { name } = req.body;
-      const hello = typeof name !== "undefined" && name !== "" ? name : "world";
+      const { name } = req.body
+      const hello = typeof name !== 'undefined' && name !== '' ? name : 'world'
 
-      return { hello };
-    },
-  );
-};
+      return { hello }
+    }
+  )
+}
 
-export default plugin;
+export default plugin
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,32 @@
 import { join } from 'node:path'
 import Fastify from 'fastify'
 import AutLoad from '@fastify/autoload'
+import Swagger from '@fastify/swagger'
+import SwaggerUI from '@fastify/swagger-ui'
 
 const fastify = Fastify({ logger: true })
+
+void fastify.register(Swagger, {
+  swagger: {
+    info: {
+      title: 'Hello World App Documentation',
+      description: 'Testing the Fastify swagger API',
+      version: '1.0.0'
+    },
+    consumes: ['application/json'],
+    produces: ['application/json'],
+    tags: [
+      {
+        name: 'Hello World',
+        description: 'You can use these routes to salute whomever you want'
+      }
+    ]
+  }
+})
+
+void fastify.register(SwaggerUI, {
+  routePrefix: '/documentation'
+})
 
 void fastify.register(AutLoad, {
   dir: join(__dirname, 'routes')
